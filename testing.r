05a7ca55b6f430e11f6b740f358344bb28fa6ccf
shagoofazahra <s.shagoofazahra@gmail.com>
2022-04-13 02:23:08 +0500

    added few lines from r studio

--- testing.r
+++ testing.r
@@ -1,2 +1,5 @@
 # This is from Git
 print (" this is from shagoofa")
+print (" these are some changes using rstudio of shagoofa zahra")
+print (" congrats for the skill you gain")
+#now this is from r studio
